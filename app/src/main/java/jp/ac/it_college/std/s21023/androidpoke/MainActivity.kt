82d188215fa2f796b3c2372cbc39a7d48cb77ff9
s21023 <s21023@std.it-college.ac.jp>
2022-12-02 00:30:53 +0900

    タイトル完成

--- app/src/main/java/jp/ac/it_college/std/s21023/androidpoke/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21023/androidpoke/MainActivity.kt
@@ -2,10 +2,32 @@ package jp.ac.it_college.std.s21023.androidpoke
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import jp.ac.it_college.std.s21023.androidpoke.databinding.ActivityMainBinding
+import java.io.InputStreamReader
+
+lateinit var pokemon: PokemonJson
+
+lateinit var pokedex: PokedexJson
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val inputStream =
+            assets?.open("ordered_pokemon.json")
+        val jsonReader = InputStreamReader(inputStream, "UTF-8").readText()
+        pokemon = Gson().fromJson(jsonReader, PokemonJson::class.java)
+
+        val inputStream2 =
+            assets?.open("filtered_pokedex.json")
+        val jsonReader2 = InputStreamReader(inputStream2, "UTF-8").readText()
+        pokedex = Gson().fromJson(jsonReader2, PokedexJson::class.java)
+
     }
+
+    override fun onBackPressed() {}
 }
